a8b2872f8b0d03e2cfd4fc31da1e0ce582141b3c
ronaldocosta <ronaldocosta.developer@gmail.com>
2022-05-31 13:18:53 -0400

    Create Home activity and layout

---
+++ app/src/main/java/br/com/lista_mercado/presentation/view/Home.kt
@@ -0,0 +1,12 @@
+package br.com.lista_mercado.presentation.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.lista_mercado.R
+
+class Home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
